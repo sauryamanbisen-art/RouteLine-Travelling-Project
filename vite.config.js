f569f985b714349f635e170a9d681c5a438f4d30
Sauryaman Bisen <sauryamanbisen@gmail.com>
2026-08-30 23:04:26 +0530

    chore: Add vite.config.js
    Detailed changes for vite.config.js.

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        trainResults: resolve(__dirname, 'train-results.html'),
+        flightResults: resolve(__dirname, 'flight-results.html'),
+        busResults: resolve(__dirname, 'bus-results.html'),
+        hotelResults: resolve(__dirname, 'hotel-results.html'),
+        metroTicket: resolve(__dirname, 'metro-ticket.html'),
+        orderFood: resolve(__dirname, 'order-food.html'),
+        trainStatus: resolve(__dirname, 'train-status.html'),
+        seatAvailability: resolve(__dirname, 'seat-availability.html')
+      }
+    }
+  }
+})
