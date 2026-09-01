65306b0719f5c9842886b905118dd5c77995029c
Sauryaman Bisen <sauryamanbisen@gmail.com>
2026-09-01 23:49:30 +0530

    chore: update vite config base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/RouteLine-Travelling-Project/',
   build: {
     rollupOptions: {
       input: {
